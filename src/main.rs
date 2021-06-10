0072ad8842c0e77bd3c3c5c3403abcbc650aebf7
youngday <you@example.com>
2021-06-10 11:41:13 +0800

    test with env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,4 @@
-/*!
-Using `env_logger`.
 
-Before running this example, try setting the `MY_LOG_LEVEL` environment variable to `info`:
-
-```no_run,shell
-export MY_LOG_LEVEL='info'
-export MY_LOG_LEVEL='trace'
-```
-
-Also try setting the `MY_LOG_STYLE` environment variable to `never` to disable colors
-or `auto` to enable them:
-
-```no_run,shell
-$ export MY_LOG_STYLE=never
-```
-*/
 
 #[macro_use]
 extern crate log;
@@ -22,9 +6,7 @@ extern crate log;
 use env_logger::Env;
 
 fn main() {
-    // The `Env` lets us tweak what the environment
-    // variables to read are and what the default
-    // value is if they're missing
+
     let env = Env::default()
         .filter_or("MY_LOG_LEVEL", "trace")
         .write_style_or("MY_LOG_STYLE", "always");
