9010f87b38707dda2d7f792553c678e3fcdb787d
youngday <young.day@aliyun.com>
2022-11-02 23:58:32 +0800

    once_cell  ,udp ,htttp to  zeromq

--- examples/zeromq-tmq.rs
+++ examples/zeromq-tmq.rs
@@ -2,19 +2,69 @@ use log::{debug, error, info, trace, warn};
 use log4rs;
 mod settings;
 use settings::Settings;
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 use futures::{SinkExt, StreamExt};
 use tmq::{publish, subscribe, Context};
-// use std::collections::HashMap;
-use std::{error::Error,  time::Duration};
+
+use std::{error::Error,fs::File,io::Read,  time::Duration};
 
 
 use tokio::time::sleep;
 
+static HASHMAP: Lazy<HashMap<i32, String>> = Lazy::new(|| {
+    let app_data: Application;
+    let filename = "examples/config/config.yaml";
+    let mut m = HashMap::new();
+    match File::open(filename) {
+        Ok(mut file) => {
+            let mut content = String::new();
+            file.read_to_string(&mut content).unwrap();
+
+            app_data = serde_yaml::from_str(&content).unwrap();
+            info!("{:?}", app_data.app.build);
+            info!("{:?}", app_data.app.environment);
+
+            let _send_ip = app_data.app.net_cfg.send_ip;
+            let _recv_ip = app_data.app.net_cfg.recv_ip;
+            let _udp_pub_topic = app_data.app.net_cfg.udp_pub_topic;
+            let _serial_pub_topic = app_data.app.net_cfg.serial_pub_topic;
+            let _http_pub_topic = app_data.app.net_cfg.http_pub_topic;
+            let _udp_sub_topic = app_data.app.net_cfg.udp_sub_topic;
+            let _serial_sub_topic = app_data.app.net_cfg.serial_sub_topic;
+            let _http_sub_topic = app_data.app.net_cfg.http_sub_topic;
+
+            info!("_send_ip:{:?}", _send_ip);
+            info!("_recv_ip:{:?}", _recv_ip);
+            info!("_udp_pub_topic:{:?}", _udp_pub_topic);
+            info!("_serial_pub_topic:{:?}", _serial_pub_topic);
+            info!("_http_pub_topic:{:?}", _http_pub_topic);
+            info!("_udp_sub_topic:{:?}", _udp_sub_topic);
+            info!("_serial_sub_topic:{:?}", _serial_sub_topic);
+            info!("_http_sub_topic:{:?}", _http_sub_topic);
+
+            m.insert(0, _send_ip.to_string());
+            m.insert(1, _recv_ip.to_string());
+            m.insert(2, _udp_pub_topic.to_string());
+            m.insert(3, _serial_pub_topic.to_string());
+            m.insert(4, _http_pub_topic.to_string());
+            m.insert(5, _udp_sub_topic.to_string());
+            m.insert(6, _serial_sub_topic.to_string());
+            m.insert(7, _http_sub_topic.to_string());
+        }
+        Err(error) => {
+            info!("There is an error {}: {}", filename, error);
+        }
+    }
+    m
+});
+
+
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Application {
-    application: Data,
+    app: Data,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
@@ -24,6 +74,7 @@ struct Data {
     environment2: Data2,
     #[serde(skip_serializing_if = "Option::is_none")]
     environment: Option<Vec<String>>,
+    net_cfg: NetCfg,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
@@ -32,10 +83,23 @@ struct Data2 {
     sec_env2: String,
 }
 
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct NetCfg {
+    send_ip: String,
+    recv_ip: String,
+    udp_pub_topic: String,
+    serial_pub_topic: String,
+    http_pub_topic: String,
+    udp_sub_topic: String,
+    serial_sub_topic: String,
+    http_sub_topic: String,
+}
+
 
 #[tokio::main] 
 async fn main() -> Result<(), Box<dyn Error>> {
     log4rs::init_file("examples/config/log.yaml", Default::default()).unwrap();
+    let version: String = "0.3.1102".to_string();
     trace!("some trace log");
     debug!("some debug log");
     info!("some information log");
@@ -46,6 +110,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // Print out our settings
     info!("{:?}", settings);
+
+
+
+
+    info!("version:{:}", version);
+
+    info!("_send_ip:{:?}", HASHMAP.get(&0));
+    info!("_recv_ip:{:?}", HASHMAP.get(&1));
 
     info!("Start your app.");
 
@@ -62,9 +134,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let recv_http_task = tokio::spawn(async {
         recv_http().await;
     });
-    let recv_test_task = tokio::spawn(async {
-        recv_test().await;
-    });
 
     let result=send_task.await.unwrap();
     info!("result:{:?}",result);
@@ -74,47 +143,43 @@ async fn main() -> Result<(), Box<dyn Error>> {
     info!("result:{:?}",result);
     let result=recv_http_task.await.unwrap();
     info!("result:{:?}",result);
-    let result=recv_test_task.await.unwrap();
-    info!("result:{:?}",result);
+
 
     Ok(())
 }
 
+
 async fn send() {
-    let bind_ip = "tcp://127.0.0.1:7898";
-    let mut socket = publish(&Context::new()).bind(bind_ip).unwrap();
+    let _bindip = HASHMAP.get(&1).unwrap().to_string(); // _recv_ip;
+    let _udp_pub_topic = HASHMAP.get(&2).unwrap().to_string(); // _udp_pub_topic;
+    let _serial_pub_topic = HASHMAP.get(&3).unwrap().to_string(); // _serial_pub_topic;
+    let _http_pub_topic = HASHMAP.get(&4).unwrap().to_string(); // _http_pub_topic;
+
+    let mut socket = publish(&Context::new()).bind(&_bindip).unwrap();
     let mut i = 0;
     loop {
         i += 1;
         let message = format!("Broadcast #{}", i);
         info!("Publish: {}", message);
+        socket.send(vec![&_udp_pub_topic, &message]).await.unwrap();
         socket
-            .send(vec![b"UDP1" as &[u8], message.as_bytes()])
+            .send(vec![&_serial_pub_topic, &message])
             .await
             .unwrap();
-        socket
-            .send(vec![b"SERIAL1" as &[u8], message.as_bytes()])
-            .await
-            .unwrap();
-        socket
-            .send(vec![b"HTTP1" as &[u8], message.as_bytes()])
-            .await
-            .unwrap();
-        socket
-            .send(vec![b"AAA" as &[u8], message.as_bytes()])
-            .await
-            .unwrap();
+        socket.send(vec![&_http_pub_topic, &message]).await.unwrap();
+        socket.send(vec!["AAA", &message]).await.unwrap();
 
-            sleep(Duration::from_millis(200)).await;
+        tokio::time::sleep(Duration::from_millis(2000)).await;
     }
 }
 
 async fn received_udp() {
-    let bind_ip = "tcp://127.0.0.1:7898";
+    let _bindip = HASHMAP.get(&0).unwrap().to_string(); // _send_ip;
+    let _udp_sub_topic = HASHMAP.get(&5).unwrap().to_string(); // _udp_sub_topic;
     let mut socket = subscribe(&Context::new())
-        .connect(bind_ip)
+        .connect(&_bindip)
         .unwrap()
-        .subscribe(b"UDP1")
+        .subscribe(_udp_sub_topic.as_bytes())
         .unwrap();
 
     while let Some(msg) = socket.next().await {
@@ -129,11 +194,12 @@ async fn received_udp() {
 }
 
 async fn recv_serial() {
-    let bind_ip = "tcp://127.0.0.1:7898";
+    let _bindip = HASHMAP.get(&0).unwrap().to_string(); //_send_ip;
+    let _serial_sub_topic = HASHMAP.get(&5).unwrap().to_string(); // _serial_sub_topic;
     let mut socket = subscribe(&Context::new())
-        .connect(bind_ip)
+        .connect(&_bindip)
         .unwrap()
-        .subscribe(b"SERIAL1")
+        .subscribe(_serial_sub_topic.as_bytes())
         .unwrap();
 
     while let Some(msg) = socket.next().await {
@@ -148,30 +214,12 @@ async fn recv_serial() {
 }
 
 async fn recv_http() {
-    let bind_ip = "tcp://127.0.0.1:7898";
+    let _bindip = HASHMAP.get(&0).unwrap().to_string(); // _send_ip;
+    let _http_sub_topic = HASHMAP.get(&5).unwrap().to_string(); // _http_sub_topic;
     let mut socket = subscribe(&Context::new())
-        .connect(bind_ip)
+        .connect(&_bindip)
         .unwrap()
-        .subscribe(b"HTTP1")
-        .unwrap();
-
-    while let Some(msg) = socket.next().await {
-        info!(
-            "Subscribe: {:?}",
-            msg.unwrap()
-                .iter()
-                .map(|item| item.as_str().unwrap_or("invalid text"))
-                .collect::<Vec<&str>>()
-        );
-    }
-}
-
-async fn recv_test() {
-    let bind_ip = "tcp://127.0.0.1:7898";
-    let mut socket = subscribe(&Context::new())
-        .connect(bind_ip)
-        .unwrap()
-        .subscribe(b"AAA")
+        .subscribe(_http_sub_topic.as_bytes())
         .unwrap();
 
     while let Some(msg) = socket.next().await {
