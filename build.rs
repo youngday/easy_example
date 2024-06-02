f9c3d5b968cc2a11921bf9b7293c81f200b6eb4d
youngday <young.day@aliyun.com>
2024-06-02 18:59:29 +0800

    grpc disabled, for github auto workflow, open build.rs and add examples if you run grpc

--- build.rs
+++ build.rs
@@ -1,15 +1,21 @@
-use std::env;
-use std::path::PathBuf;
+// use std::env;
+// use std::path::PathBuf;
 
+// fn main() {
+    //grpc 
+
+    // let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    // tonic_build::configure()
+    //     .file_descriptor_set_path(out_dir.join("helloworld_descriptor.bin"))
+    //     .compile(&["proto/helloworld/helloworld.proto"], &["proto"])
+    //     .unwrap();
+    // // tonic_build::compile_protos("proto/echo/echo.proto").unwrap();
+    // tonic_build::configure()
+    //     .compile(&["proto/echo/echo.proto"], &["proto"])
+    //     .unwrap();
+
+// }
+//no grpc
 fn main() {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    tonic_build::configure()
-        .file_descriptor_set_path(out_dir.join("helloworld_descriptor.bin"))
-        .compile(&["proto/helloworld/helloworld.proto"], &["proto"])
-        .unwrap();
-    // tonic_build::compile_protos("proto/echo/echo.proto").unwrap();
-    tonic_build::configure()
-        .compile(&["proto/echo/echo.proto"], &["proto"])
-        .unwrap();
 
 }
