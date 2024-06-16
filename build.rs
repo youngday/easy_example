7533bd245b49e7eabe41f1bff751e83cc65f3101
youngday <young.day@aliyun.com>
2024-06-16 20:02:25 +0800

    proto rust.yml workflow

--- build.rs
+++ build.rs
@@ -1,11 +1,18 @@
 // please uncomment for grpc
-// use std::io::Result;
+use std::io::Result;
+use std::env;
+use std::path::PathBuf;
 
-// use poem_grpc_build::Config;
+use poem_grpc_build::Config;
 
-// fn main() -> Result<()> {
-//     Config::new()
-//         .codec("::poem_grpc::codec::JsonCodec")
-//         .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
-//         .compile(&["./proto/helloworld.proto"], &["./proto"])
-// }
+fn main() -> Result<()> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    Config::new()
+        .codec("::poem_grpc::codec::JsonCodec")
+        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .file_descriptor_set_path(out_dir.join("helloworld_descriptor.bin"))
+        .compile(&["./proto/helloworld.proto"], &["./proto"])
+}
+
+
+
